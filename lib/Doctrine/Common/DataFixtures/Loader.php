9aa78432add8e4bfc356b829ce3c443124514a07
Javier Lorenzana <javier.lorenzana@gointegro.com>
2014-06-02 17:42:11 -0300

    Añadiendo la posibilidad de cargar fixtures de archivos.

--- lib/Doctrine/Common/DataFixtures/Loader.php
+++ lib/Doctrine/Common/DataFixtures/Loader.php
@@ -49,7 +49,7 @@ class Loader
      * @var boolean
      */
     private $orderFixturesByNumber = false;
-    
+
     /**
      * Determines if we must order fixtures by its dependencies
      *
@@ -93,11 +93,11 @@ class Loader
             $includedFiles[] = $sourceFile;
         }
         $declared = get_declared_classes();
-        
+
         foreach ($declared as $className) {
             $reflClass = new \ReflectionClass($className);
             $sourceFile = $reflClass->getFileName();
-            
+
             if (in_array($sourceFile, $includedFiles) && ! $this->isTransient($className)) {
                 $fixture = new $className;
                 $fixtures[] = $fixture;
@@ -105,6 +105,20 @@ class Loader
             }
         }
         return $fixtures;
+    }
+
+    /**
+     * Find fixtures classes in a given file and load them.
+     *
+     * @param string $path File to find fixture classes in.
+     * @return array $fixtures Array of loaded fixture object instances
+     */
+    public function loadFromFile($pathName)
+    {
+        $sourceFile = realpath($pathName);
+        require_once $sourceFile;
+
+        return $this->loadDeclaredFixtures([$sourceFile]);
     }
 
     /**
@@ -130,7 +144,7 @@ class Loader
 
         if (!isset($this->fixtures[$fixtureClass])) {
             if ($fixture instanceof OrderedFixtureInterface && $fixture instanceof DependentFixtureInterface) {
-                throw new \InvalidArgumentException(sprintf('Class "%s" can\'t implement "%s" and "%s" at the same time.', 
+                throw new \InvalidArgumentException(sprintf('Class "%s" can\'t implement "%s" and "%s" at the same time.',
                     get_class($fixture),
                     'OrderedFixtureInterface',
                     'DependentFixtureInterface'));
@@ -163,7 +177,7 @@ class Loader
         if ($this->orderFixturesByDependencies) {
             $this->orderFixturesByDependencies();
         }
-        
+
         if (!$this->orderFixturesByNumber && !$this->orderFixturesByDependencies) {
             $this->orderedFixtures = $this->fixtures;
         }
@@ -188,7 +202,7 @@ class Loader
 
     /**
      * Orders fixtures by number
-     * 
+     *
      * @todo maybe there is a better way to handle reordering
      * @return void
      */
@@ -209,22 +223,22 @@ class Loader
             return 0;
         });
     }
-    
-    
+
+
     /**
      * Orders fixtures by dependencies
-     * 
+     *
      * @return void
      */
     private function orderFixturesByDependencies()
     {
         $sequenceForClasses = array();
 
-        // If fixtures were already ordered by number then we need 
+        // If fixtures were already ordered by number then we need
         // to remove classes which are not instances of OrderedFixtureInterface
         // in case fixtures implementing DependentFixtureInterface exist.
         // This is because, in that case, the method orderFixturesByDependencies
-        // will handle all fixtures which are not instances of 
+        // will handle all fixtures which are not instances of
         // OrderedFixtureInterface
         if ($this->orderFixturesByNumber) {
             $count = count($this->orderedFixtures);
@@ -244,7 +258,7 @@ class Loader
                 continue;
             } elseif ($fixture instanceof DependentFixtureInterface) {
                 $dependenciesClasses = $fixture->getDependencies();
-                
+
                 $this->validateDependencies($dependenciesClasses);
 
                 if (!is_array($dependenciesClasses) || empty($dependenciesClasses)) {
@@ -254,7 +268,7 @@ class Loader
                 if (in_array($fixtureClass, $dependenciesClasses)) {
                     throw new \InvalidArgumentException(sprintf('Class "%s" can\'t have itself as a dependency', $fixtureClass));
                 }
-                
+
                 // We mark this class as unsequenced
                 $sequenceForClasses[$fixtureClass] = -1;
             } else {
@@ -266,7 +280,7 @@ class Loader
         // Now we order fixtures by sequence
         $sequence = 1;
         $lastCount = -1;
-        
+
         while (($count = count($unsequencedClasses = $this->getUnsequencedClasses($sequenceForClasses))) > 0 && $count !== $lastCount) {
             foreach ($unsequencedClasses as $key => $class) {
                 $fixture = $this->fixtures[$class];
@@ -275,29 +289,29 @@ class Loader
 
                 if (count($unsequencedDependencies) === 0) {
                     $sequenceForClasses[$class] = $sequence++;
-                }                
+                }
             }
-            
+
             $lastCount = $count;
         }
 
         $orderedFixtures = array();
-        
-        // If there're fixtures unsequenced left and they couldn't be sequenced, 
+
+        // If there're fixtures unsequenced left and they couldn't be sequenced,
         // it means we have a circular reference
         if ($count > 0) {
             $msg = 'Classes "%s" have produced a CircularReferenceException. ';
             $msg .= 'An example of this problem would be the following: Class C has class B as its dependency. ';
             $msg .= 'Then, class B has class A has its dependency. Finally, class A has class C as its dependency. ';
             $msg .= 'This case would produce a CircularReferenceException.';
-            
+
             throw new CircularReferenceException(sprintf($msg, implode(',', $unsequencedClasses)));
         } else {
             // We order the classes by sequence
             asort($sequenceForClasses);
 
             foreach ($sequenceForClasses as $class => $sequence) {
-                // If fixtures were ordered 
+                // If fixtures were ordered
                 $orderedFixtures[] = $this->fixtures[$class];
             }
         }
@@ -308,7 +322,7 @@ class Loader
     private function validateDependencies($dependenciesClasses)
     {
         $loadedFixtureClasses = array_keys($this->fixtures);
-        
+
         foreach ($dependenciesClasses as $class) {
             if (!in_array($class, $loadedFixtureClasses)) {
                 throw new \RuntimeException(sprintf('Fixture "%s" was declared as a dependency, but it should be added in fixture loader first.', $class));
@@ -333,5 +347,5 @@ class Loader
         }
 
         return $unsequencedClasses;
-    }           
+    }
 }
